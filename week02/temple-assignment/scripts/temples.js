6b01e466434000bcf852c08045afb263fa00e06a
IsaacIhoon-python <ihoonisaac@gmail.com>
2026-03-23 08:23:20 +0100

    modified to actualize responsive behavior

--- week02/temple-assignment/scripts/temples.js
+++ week02/temple-assignment/scripts/temples.js
@@ -1,12 +1,23 @@
+// Navigation Elements
 const menuBtn = document.querySelector("#menuBtn");
 const navMenu = document.querySelector("#navMenu");
 
-menuBtn.addEventListener("click", () => {
-  navMenu.classList.toggle("open");
-});
+// Toggle Navigation Menu
+if (menuBtn && navMenu) {
+  menuBtn.addEventListener("click", () => {
+    navMenu.classList.toggle("open");
+  });
+}
 
-// Footer dates
-document.querySelector("#year").textContent = new Date().getFullYear();
+// Footer Elements
+const year = document.querySelector("#year");
+const lastModified = document.querySelector("#lastModified");
 
-document.querySelector("#lastModified").textContent =
-"Last Updated: " + document.lastModified;
+// Set Dynamic Footer Content
+if (year) {
+  year.textContent = new Date().getFullYear();
+}
+
+if (lastModified) {
+  lastModified.textContent = "Last Updated: " + document.lastModified;
+}
